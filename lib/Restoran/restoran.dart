119848176dd137c1f0a70c66bf3e7dfa20fb935d
Muhammet Emin AYDINALP <muhammetemin4444@gmail.com>
2025-03-25 10:19:11 +0300

    restoran

---
+++ lib/Restoran/restoran.dart
@@ -0,0 +1,9 @@
+/*
+Ana sayfada restoranlar listelensin.
+Restoran ismi, adresi ve kaç puan aldığı bilgileri görünsün.
+Herhangi bir restoranta tıklandığında o restoranda
+bulunan menüler listelensin.
+Menü de şu şekilde :
+Memü ismi ve fiyat bilgisi görünsün.
+
+ */
